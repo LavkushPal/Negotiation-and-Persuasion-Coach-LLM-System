8b724c04f24d313dfaf81139bee3143813d5d292
LavkushPal <lavkushpal134@gmail.com>
2026-03-08 12:24:00 +0530

    feat(backend): model selction,base model inference, finetunig knowledge refinement

---
+++ app/backend/finetuning.py
@@ -0,0 +1,62 @@
+# !pip install -q accelerate peft bitsandbytes transformers trl
+# pip install trl
+# !hf auth login
+
+
+import os
+import torch
+import tensorflow as tf
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
+
+import transformers
+
+
+
+# ...........................inference before pre-finetuning (hugging-face)
+
+# model_id = "meta-llama/Llama-3.1-8B"
+model_id = "Qwen/Qwen2.5-7B-Instruct"
+
+pipeline = transformers.pipeline(
+    "text-generation", 
+    model=model_id, 
+    model_kwargs={
+        "torch_dtype": torch.bfloat16
+    }, 
+    device_map="auto"
+)
+
+print(pipeline("Hey how are you doing today?"))
+
+#...............................inference locally
+
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B") #download locally base model
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B") #download locally tokenizaer for base model
+
+model.save_pretrained("./llama3_model")
+tokenizer.save_pretrained("./llama3_model")
+
+# inference
+prompt = "Explain transformers in simple words"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+outputs = model.generate(
+    **inputs,
+    max_new_tokens=150,
+    temperature=0.7,
+    top_p=0.9,
+    do_sample=True
+)
+
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
--- readme.md
+++ readme.md
@@ -1,3 +1,26 @@
 <h1> Negotiation Coach LLM </h1>
 
+steps done --
 1. setup and configuration 
+2. base model selection : Llama-3.1-8B
+3. finetuning knowledge refinement
+4. baseline inference (require access permission)
+5. 
+
+
+
+
+steps to follow --
+
+1. Setup environment
+2. Choose base model
+3. Run baseline inference
+4. Collect datasets
+5. Design dataset schema
+6. Clean & preprocess dataset
+7. Fine-tune using QLoRA
+8. Evaluate base vs fine-tuned model
+9. Deploy with vLLM
+10. Build FastAPI backend
+11. Build UI
+12. Performance optimization
